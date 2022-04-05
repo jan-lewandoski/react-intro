4c842097903b47251c41af14d5847949719f21f0
Jan Lewandoski <jan.lewandoski@htdevelopers.com>
2022-04-05 22:10:10 +0200

    Add hooks

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,16 +1,42 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [workshops, setWorkshops] = useState([]);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    // Gdy komponent się wyrenderuje, sprawdz czy są jakieś warsztaty w pamięci przegladarki i dodaj je do stanu
+
+    const workshopsStr = localStorage.getItem("workshops");
+
+    if (workshopsStr) {
+      const workshopsArr = workshopsStr.split(",");
+      setWorkshops(workshopsArr);
+    }
+  }, []);
+
+  useEffect(() => {
+    // Gdy dodamy nowe warsztaty, zapisz je w pamięci przeglądarki
+
+    if (workshops.length > 0) {
+      localStorage.setItem("workshops", workshops);
+    }
+  }, [workshops]);
+
   const handleClick = () => {
-    console.log("Dodano warsztaty!");
+    setWorkshops([...workshops, name]);
+    setName("");
   };
 
   const handleChange = (event) => {
-    console.log("Nazwa warsztatów: ", event.target.value);
+    setName(event.target.value);
   };
 
   return (
     <div>
-      <input onChange={handleChange} />
+      <input value={name} onChange={handleChange} />
       <button onClick={handleClick}>Dodaj warsztaty</button>
+      <p>{workshops.join(",")}</p>
     </div>
   );
 }
