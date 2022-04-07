31fcc4972e6ab9625cd127bf820b6bdf3cb7ed07
Jan Lewandoski <jan.lewandoski@htdevelopers.com>
2022-04-07 14:49:37 +0200

    Hooks

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,19 +1,43 @@
+import { useState, useEffect } from "react";
 import logo from "../../logo.svg";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [workshops, setWorkshops] = useState([]);
+
+  useEffect(() => {
+    // Gdy komponent sie wyrenderuje, pobierzmy warsztaty z pamięci przeglądarki
+    const workshopsStr = localStorage.getItem("workshops");
+
+    if (workshopsStr) {
+      const workshapsArr = workshopsStr.split(",");
+      setWorkshops(workshapsArr);
+    }
+  }, []);
+
+  useEffect(() => {
+    // Za kazdym razem, gdy zmieni sie lista warsztatów - zapiszmy je w pamięci przeglądarki
+    if (workshops.length > 0) {
+      localStorage.setItem("workshops", workshops);
+    }
+  }, [workshops]);
+
   const addWorkshop = () => {
-    console.log("Dodaj warsztat");
+    setWorkshops([...workshops, name]);
+    setName("");
   };
 
   const onChange = (event) => {
-    console.log(event.target.value);
+    setName(event.target.value);
   };
 
   return (
     <div>
-      <input onChange={onChange}></input>
+      <p>Nazwa warsztatów: {name}</p>
+      <input value={name} onChange={onChange}></input>
       <button onClick={addWorkshop}>Dodaj warsztaty!</button>
+      <div>{workshops.join(",")}</div>
     </div>
   );
 }
